b0d643d3e406a8529b5c6eab411e9897bc960d2d
Tetiana Surkova <tatianasurkovaa@gmail.com>
2025-02-05 20:37:47 +0200

    refactor: update InputField props

--- src/shared/ui/input-field/InputField.tsx
+++ src/shared/ui/input-field/InputField.tsx
@@ -3,12 +3,23 @@ import clsx from 'clsx';
 
 interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  id: string;
+  name: string;
   icon?: ReactNode;
   wrapperClassName?: string;
   labelClassName?: string;
 }
 
-const InputField: FC<InputFieldProps> = ({ label, icon, wrapperClassName, labelClassName, className, ...props }) => {
+const InputField: FC<InputFieldProps> = ({
+  label,
+  icon,
+  wrapperClassName,
+  labelClassName,
+  className,
+  id,
+  name,
+  ...props
+}) => {
   return (
     <div className={clsx('relative flex items-center', wrapperClassName)}>
       <label
@@ -16,11 +27,14 @@ const InputField: FC<InputFieldProps> = ({ label, icon, wrapperClassName, labelC
           'absolute left-[42px] top-3 text-[10px] font-bold uppercase tracking-[0.3em] text-background',
           labelClassName
         )}
+        htmlFor={id}
       >
         {label}
       </label>
       <input
         {...props}
+        id={id}
+        name={name}
         className={clsx(
           'w-full rounded-full border-2 border-light-text bg-transparent pb-3.5 pl-10 pt-6 text-light-text',
           'placeholder:text-light-text placeholder:opacity-50',
