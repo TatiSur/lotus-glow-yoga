7b3125a24eaf86d979e3a1a6438507ad270d2bcb
Tetiana Surkova <tatianasurkovaa@gmail.com>
2025-02-13 23:59:00 +0200

    fix: MenuButton position on large screens

--- src/widgets/header/ui/header/Header.tsx
+++ src/widgets/header/ui/header/Header.tsx
@@ -6,7 +6,7 @@ const Header: FC = () => {
   return (
     <header className="header main-container relative z-10 flex justify-between">
       <Logo className="mb-3 mt-8 md:mb-0" />
-      <MenuButton className="header fixed right-5 z-50 2xs:right-10 lg:right-[60px]" />
+      <MenuButton className="header fixed right-5 z-50 2xs:right-10 lg:right-[60px] 2xl:inset-x-0 2xl:mx-auto 2xl:translate-x-[606px]" />
     </header>
   );
 };
