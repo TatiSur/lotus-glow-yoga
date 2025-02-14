d74b8c08d1e31403b7af0a6d1a8d82ae9b4f163c
Tetiana Surkova <tatianasurkovaa@gmail.com>
2025-02-14 17:55:00 +0200

    fix: header and hero section height

--- src/widgets/header/ui/header/Header.tsx
+++ src/widgets/header/ui/header/Header.tsx
@@ -5,7 +5,7 @@ import MenuButton from '../menu-button/MenuButton';
 const Header: FC = () => {
   return (
     <header className="header main-container relative z-10 flex justify-between">
-      <Logo className="mb-3 mt-8 md:mb-0" />
+      <Logo className="mb-3 mt-4 md:mb-0 md:mt-8" />
       <MenuButton className="header fixed right-5 z-50 2xs:right-10 lg:right-[60px] 2xl:inset-x-0 2xl:mx-auto 2xl:translate-x-[606px]" />
     </header>
   );
