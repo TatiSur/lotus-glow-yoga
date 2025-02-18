1496fac12a5b4014cfa20b9d2ce19d240c0f269d
Tetiana Surkova <tatianasurkovaa@gmail.com>
2025-02-18 14:54:00 +0200

    feat: show BookSessionModal by SearchParam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import { Header } from '@/widgets/header';
 import { HeroSection } from '@/widgets/hero-section';
 import { AboutUsSection } from '@/widgets/about-us-section';
@@ -6,8 +8,15 @@ import { BookSessionBanner } from '@/widgets/book-session-banner';
 import { InstructorsSection } from '@/widgets/instructors-section';
 import { SubscriptionSection } from '@/widgets/subscription-section';
 import { Footer } from '@/widgets/footer';
+import { BookSessionModal } from '@/features/book-session';
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const { modal } = await searchParams;
+
   return (
     <>
       <Header />
@@ -20,6 +29,9 @@ export default function Home() {
         <SubscriptionSection />
       </main>
       <Footer />
+      <Suspense fallback={<div>Loading...</div>}>
+        {modal === 'book-session' && <BookSessionModal />}
+      </Suspense>
     </>
   );
 }
